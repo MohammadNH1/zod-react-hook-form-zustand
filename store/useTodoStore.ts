6f9d543a253809583d5436eedec8fa39ef9c232a
MohammadNH1 <mustafasheikh28864@gmail.com>
2025-10-15 15:01:17 +0600

    update useTodoStore

--- store/useTodoStore.ts
+++ store/useTodoStore.ts
@@ -1,24 +1,25 @@
 import { todoType } from '@/types/todoTypes';
 import {create} from 'zustand';
 
-type TodoStore = {
-  todos: todoType[];
-  page: number;
-  total: number;
-  setTodos: (todos: todoType[], total: number) => void;
-  setPage: (page: number) => void;
+
+type todoStore = {
+  todos:todoType[];
+  page:number;
+  total:number;
+  setTodos:(todos:todoType[], total:number)=>void;
+  setPage:(page:number)=>void;
   isModalOpen:boolean;
   openModal:()=>void;
   closeModal:()=>void;
-};
+}
 
-export const useTodoStore = create<TodoStore>((set) => ({
-  todos: [],
-  page: 1,
-  total: 0,
-  setTodos: (todos, total) => set({ todos, total }),
-  setPage: (page) => set({ page }),
-  isModalOpen: false,
+export const useTodoStore = create<todoStore>((set)=>({
+  todos:[],
+  page:1,
+  total:0,
+  setTodos:(todos,total)=>set({todos,total}),
+  setPage:(page)=>set({page}),
+  isModalOpen:false,
   openModal:()=>set({isModalOpen:true}),
   closeModal:()=>set({isModalOpen:false})
-}));
+}))
